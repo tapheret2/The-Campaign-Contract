6451338475ecfdea9a751a2076c467b7d8a23629
Phạm Tiến Phát <phamtienphat2006@gmail.com>
2025-11-15 15:32:27 +0700

    feat: add hot-update files for webpack and index page
    - Introduced new hot-update files for the index page and various webpack configurations.
    - Updated the index page to include campaign rendering logic and improved component structure.
    - Enhanced webpack runtime modules to support compatibility and hash retrieval.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f5399619ff5b5295"; }
+/******/ 		__webpack_require__.h = function() { return "009e84d90f924688"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.b187291a556686be.hot-update.js
+++ .next/static/webpack/webpack.b187291a556686be.hot-update.js
@@ -23,7 +23,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "6eddb88ed0935233"; }
+/******/ 	__webpack_require__.h = function() { return "b19f39e10d66b481"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
